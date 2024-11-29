48737ace5df946a42b6f7758e9506b1515dcc69a
lautaromdelgado <lautaromdelgado@gmail.com>
2024-11-29 01:45:34 -0300

    Controlador para crear tarea (faltan validaciones en campos dónde haya relación con otras tablas como idUsuario)

--- controllers/tasks/tasks.go
+++ controllers/tasks/tasks.go
@@ -2,6 +2,8 @@ package tasks
 
 import (
 	"net/http"
+	"taskgo/database"
+	"taskgo/models"
 
 	"github.com/labstack/echo/v4"
 )
@@ -12,7 +14,38 @@ type ResponseMessage struct {
 	Message string `json:"message,omitempty"`
 }
 
+type Data struct {
+	Task *models.Tasks
+}
+
 func CreateTask(c echo.Context) error {
+	db := database.GetDB()
+	task := new(models.Tasks)
+
+	if err := c.Bind(&task); err != nil {
+		return c.JSON(http.StatusBadRequest, ResponseMessage{
+			Status:  "error",
+			Message: "invalid request " + err.Error(),
+		})
+	}
+
+	if task.Title == "" || task.Nivel < 0 {
+		return c.JSON(http.StatusBadRequest, ResponseMessage{
+			Status:  "error",
+			Message: "cant have empty fields",
+		})
+	}
+
+	if err := db.Create(&task); err != nil {
+		c.JSON(http.StatusBadRequest, ResponseMessage{
+			Status:  "error",
+			Message: "error creating task",
+		})
+	}
+
+	data := Data{
+		Task: task,
+	}
 
 	return c.JSON(http.StatusOK, ResponseMessage{
 		Status: "success",
